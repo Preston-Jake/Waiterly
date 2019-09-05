ef61b185ed16ebe4afbb6873fb1186b1e27a4643
Jake-Preston <prestonjak@outlook.com>
2019-09-05 14:53:06 -0500

    Add project files.

---
+++ Waiterly/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Waiterly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
