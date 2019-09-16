86444d8a69757a361196729a99a46968a9a0ea3d
Jake-Preston <prestonjak@outlook.com>
2019-09-16 11:45:08 -0500

    finished user tables

--- Waiterly/Models/UserTable.cs
+++ Waiterly/Models/UserTable.cs
@@ -12,16 +12,19 @@ namespace Waiterly.Models
         [Required]
         public int Id { get; set; }
 
+        [Display(Name = "Waiter")]
         public string UserId { get; set; } = null;
         public ApplicationUser User { get; set; }
 
         [Required]
+        [Display(Name = "Table Number")]
         public int TableNumber { get; set; }
 
         [Required]
         public int Seats { get; set; }
 
         [Required]
+        [Display(Name = "Restaurant")]
         public int RestaurantId { get; set; }
         public Restaurant Restaurant { get; set; }
 
